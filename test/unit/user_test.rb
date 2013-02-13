0669dc438e18d9ae557ca654d2587250861a80ad
jesusalc <jesusalc@gmail.com>
2013-02-13 15:11:09 -0800

    Adding tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,34 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = 'jesusalc'
+    user.first_name = 'Jesus'
+    user.last_name = "Alc"
+    user.password = "machoman"
+    user.password_confirmation = "machoman"
+    user.email = "jesusalc@yahoo.com"
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
 end
