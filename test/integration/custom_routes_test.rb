b7ec18c723996dd0e420d0f507986e31a309f758
jesusalc <jesusalc@gmail.com>
2013-02-13 18:20:33 -0800

    Added logout redirect integration test

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -7,5 +7,16 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   test " that /login route open the loign page"  do
     get '/login'
     assert_response :success 
+  end 
+  
+  test " that /register route open the loign page"  do
+    get '/register'
+    assert_response :success 
+  end
+  
+  test "that /logout route open the log out page" do
+    get '/logout'
+    assert_response :redirect
+    assert_redirected_to '/'
   end
 end
